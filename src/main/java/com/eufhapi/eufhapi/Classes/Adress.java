1fb9676cdd2629e4fa659dd96d7defa823dc3c9c
Julian Pasqué <julian.pasque@xarvio.com>
2023-05-11 09:20:02 +0200

    changes

--- src/main/java/com/eufhapi/eufhapi/Classes/Adress.java
+++ src/main/java/com/eufhapi/eufhapi/Classes/Adress.java
@@ -8,7 +8,7 @@ public class Adress {
         setCity(_city);
         setPostCode(_postCode);
         setHouseNumber(_houseNumber);
-        setStreet(_street);
+        setStreet(_houseNumber);
     }
 
     // Properties
